cbb5a63b940e87f9fef610acc46d99084499fe5e
Jose Gomes <jgomestkd@gmail.com>
2025-09-21 16:47:01 -0300

    Atualizando pom e adicionando as migration

---
+++ startup/src/main/resources/db/migration/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO person (address, first_name, gender, last_name) VALUES
+                                                                ('São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+                                                                ('Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+                                                                ('Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+                                                                ('Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+                                                                ('Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
+                                                                ('Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+                                                                -- Note que os valores duplicados foram removidos para evitar entradas idênticas
+                                                                ('Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
